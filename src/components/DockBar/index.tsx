457508ab4046edefe3983f61526f13281f396d12
haesoo-y <haesoo9410@gmail.com>
2023-03-20 21:53:26 +0900

    feat: 독바 컴포넌트 추가

---
+++ src/components/DockBar/index.tsx
@@ -0,0 +1,17 @@
+import React, { useState, useEffect } from 'react';
+import { IconButton } from '@components';
+import S from './styled';
+
+const DockBar = () => {
+  return (
+    <S.DockBarWrapper>
+      <IconButton icon="A" />
+      <IconButton icon="B" />
+      <IconButton icon="C" />
+      <IconButton icon="D" />
+      <IconButton icon="E" />
+    </S.DockBarWrapper>
+  );
+};
+
+export default DockBar;
