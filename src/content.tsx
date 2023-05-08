736f2f1667d5cba1b8eb8a64311399f931d351e3
kukucorn <hyeonku.dev@gmail.com>
2023-05-08 22:46:07 +0900

    Add custom cursor

--- src/content.tsx
+++ src/content.tsx
@@ -12,12 +12,27 @@ const stampMarkImageUrl = chrome.runtime.getURL('stamp_mark.png');
 console.log(stampImageUrl);
 
 const paper = document.createElement('div');
-paper.style.zIndex = '9999';
-paper.style.cursor = `url("${stampImageUrl}") 40 70, help`;
+paper.style.zIndex = '9998';
+// paper.style.cursor = `url("${stampImageUrl}") 40 70, help`;
+paper.style.cursor = `none`;
 paper.style.display = 'flex';
 paper.style.position = 'fixed';
 paper.style.top = '0';
 paper.style.left = '0';
+
+const cursorPlayground = document.createElement('cursorPlayground');
+cursorPlayground.style.zIndex = '9999';
+cursorPlayground.style.display = 'flex';
+cursorPlayground.style.position = 'fixed';
+cursorPlayground.style.top = '0';
+cursorPlayground.style.left = '0';
+cursorPlayground.style.width = '100%';
+cursorPlayground.style.height = '100%';
+cursorPlayground.style.cursor = 'none';
+
+const customCursor = new Image(70, 70);
+customCursor.src = `${stampImageUrl}`;
+cursorPlayground.appendChild(customCursor);
 
 var stage = new Konva.Stage({
   container: paper,
@@ -28,6 +43,17 @@ var layer = new Konva.Layer();
 stage.add(layer);
 
 const MARK_SIZE = 100;
+
+const handleMouseMove = (event: any) => {
+  event.preventDefault();
+
+  const mouseY = event.clientY;
+  const mouseX = event.clientX;
+
+  console.log(`${mouseX} ${mouseY}`);
+
+  customCursor.style.transform = `translate3d(${mouseX - 35}px, ${mouseY - 50}px, 0)`;
+};
 
 const handleMouseClick = (event: any) => {
   event.preventDefault();
@@ -46,11 +72,35 @@ const handleMouseClick = (event: any) => {
       height: MARK_SIZE,
     });
     cropStamp(image);
-    layer.add(image);
     const curPointerPos = stage.getPointerPosition() as Vector2d;
-    image.position({ x: curPointerPos.x - MARK_SIZE / 2, y: curPointerPos.y - MARK_SIZE / 2 });
+    setTimeout(() => {
+      layer.add(image);
+      image.position({ x: curPointerPos.x - MARK_SIZE / 2, y: curPointerPos.y - MARK_SIZE / 2 });
+    }, 100);
   };
   imageObj.src = stampMarkImageUrl;
+};
+
+const mouseMove = (event: any) => {
+  const mouseY = event.clientY;
+  const mouseX = event.clientX;
+
+  customCursor.animate(
+    [
+      {
+        transform: `translate3d(${mouseX - 35}px, ${mouseY - 90}px, 0)`,
+      },
+      {
+        transform: `translate3d(${mouseX - 35}px, ${mouseY - 50}px, 0)`,
+      },
+      {
+        transform: `translate3d(${mouseX - 35}px, ${mouseY - 90}px, 0)`,
+      },
+      // { opacity: 1, easing: 'ease-out' },
+      // { opacity: 0.1, easing: 'ease-in' },
+    ],
+    { duration: 300, iterations: 1 },
+  );
 };
 
 let isEnabled = false;
@@ -60,11 +110,16 @@ chrome.runtime.onMessage.addListener(message => {
     console.log('receive on!');
     isEnabled = true;
     document.body.appendChild(paper);
+    document.body.appendChild(cursorPlayground);
     document.body.addEventListener('click', handleMouseClick);
+    document.body.addEventListener('mousemove', handleMouseMove);
+    document.body.addEventListener('click', mouseMove);
     console.log('hot reloader!');
   } else if (message === 'off' && isEnabled === true) {
     isEnabled = false;
     document.body.removeEventListener('click', handleMouseClick);
+    document.body.removeEventListener('mousemove', handleMouseMove);
+    document.body.removeEventListener('click', mouseMove);
     layer.find('Image').forEach(image => {
       image.destroy();
     });
