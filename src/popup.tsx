6cc07fe4804a538e02f42322879fb62e37f64a93
rudy3091 <grayblack313@gmail.com>
2023-06-24 23:00:27 +0900

    chore: update `src/index.tsx` & `src/popup.tsx` to support React:18

--- src/popup.tsx
+++ src/popup.tsx
@@ -1,7 +1,16 @@
 import * as React from 'react';
-import * as ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 
 import PopupApp from './PopupApp';
 
-const $popup = document.getElementById('popup');
-ReactDOM.render(<PopupApp />, $popup);
+const $popup = document.createElement('div') as HTMLElement;
+$popup.id = 'popup';
+document.body.appendChild($popup);
+
+document.addEventListener('DOMContentLoaded', () => {
+  ReactDOM.createRoot(document.querySelector('#popup')!).render(
+    <React.StrictMode>
+      <PopupApp />
+    </React.StrictMode>,
+  );
+});
