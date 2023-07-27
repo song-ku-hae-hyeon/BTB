c924647ddc224eb78318a78eab0a46628ca6dc6f
haesoo-y <haesoo9410@gmail.com>
2023-07-27 11:55:32 +0900

    feat: 마우스에 형광펜 적용

---
+++ src/components/Highlighter/styled.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const HighlighterWrapper = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 9000;
+  cursor: url(highlighter.png) 8 64, auto;
+`;
+
+export default { HighlighterWrapper };
