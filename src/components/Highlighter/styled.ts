e9a8a7b5370a9a61d710d6160b1d039dcdd6f87d
haesoo-y <haesoo9410@gmail.com>
2023-07-28 10:23:15 +0900

    fix: build후 이미지 못불러오던 오류 수정

--- src/components/Highlighter/styled.ts
+++ src/components/Highlighter/styled.ts
@@ -1,11 +1,15 @@
 import styled from 'styled-components';
+// @ts-ignore
+import img from '../../../public/highlighter.png';
+
+const imgUrl = img ?? 'highlighter.png';
 
 const HighlighterWrapper = styled.div`
   position: fixed;
   top: 0;
   left: 0;
   z-index: 9000;
-  cursor: url(highlighter.png) 8 64, auto;
+  cursor: url(${imgUrl}) 8 64, auto;
 `;
 
 export default { HighlighterWrapper };
