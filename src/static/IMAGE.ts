726b1dd8ee0582366c07a8402cd3f6940e9d7b76
haesoo-y <haesoo9410@gmail.com>
2023-09-10 00:18:52 +0900

    feat: 독바 스타일링

--- src/static/IMAGE.ts
+++ src/static/IMAGE.ts
@@ -2,13 +2,16 @@
 import highlighter from '@public/highlighter_64.png';
 import stamp from '@public/stamp_64.png';
 import stamp_mark from '@public/stamp_mark.png';
+import ant from '@public/ant1_64.png';
 
 const HIGHLIGHTER_URL = highlighter;
 const STAMP_URL = stamp;
 const STAMP_MARK_URL = stamp_mark;
+const ANT_URL = ant;
 
 export default {
   HIGHLIGHTER_URL,
   STAMP_URL,
   STAMP_MARK_URL,
+  ANT_URL,
 };
