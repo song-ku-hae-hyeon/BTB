6ae67954d6254e8f8aec02c96ab644ca80de001d
정현구 <hyeonku.dev@gmail.com>
2023-09-11 23:52:50 +0900

    fix: stamp (#19)
    * temp
    
    * Add stamp cursor
    
    * Move cursor position

--- src/components/Stamp/index.tsx
+++ src/components/Stamp/index.tsx
@@ -1,18 +1,19 @@
 import { useState, useEffect, useRef, RefObject } from 'react';
 import Konva from 'konva';
 import { Layer, Image as KonvaImage } from 'react-konva';
-import S from './styled';
 
 import { IMAGE } from '@static';
 
-import { IRect, Vector2d } from 'konva/lib/types';
+import { Vector2d } from 'konva/lib/types';
 import { useRecoilState } from 'recoil';
 import { StampAtom } from '@recoil';
 import { useAntKiller } from '../../hooks/useAntKiller';
 
 const MARK_SIZE = 100;
-const imageObj = new Image();
-imageObj.src = IMAGE.STAMP_MARK_URL;
+const stampMarkImage = new Image();
+stampMarkImage.src = IMAGE.STAMP_MARK_URL;
+const stampImage = new Image();
+stampImage.src = IMAGE.STAMP_URL;
 
 type PaperProps = {
   stageRef: RefObject<Konva.Stage> | null;
@@ -20,12 +21,16 @@ type PaperProps = {
 
 const Paper = ({ stageRef }: PaperProps) => {
   const [stampPositions, setStampPositions] = useRecoilState(StampAtom);
-  const imgRef = useRef<HTMLImageElement>(null);
+  const layerRef = useRef<Konva.Layer>(null);
   const { ants, killIfInRange } = useAntKiller(MARK_SIZE, MARK_SIZE);
 
   useEffect(() => {
     const stage = stageRef?.current;
     if (!stage) return;
+
+    const container = stage.container();
+
+    container.style.cursor = `url(${IMAGE.STAMP_URL}) 30 56, auto`;
 
     const handleMouseDown = ({ evt: { clientX, clientY } }: Konva.KonvaEventObject<MouseEvent>) => {
       const drawStampMark = (clientX: number, clientY: number) => {
@@ -33,55 +38,49 @@ const Paper = ({ stageRef }: PaperProps) => {
         setStampPositions(prevArray => [...prevArray, { ...curPointerPos, cropRect: cropStamp() }]);
       };
 
-      const stampSeal = (clientX: number, clientY: number) => {
-        if (imgRef.current) {
-          // imgRef.current.animate(
-          //   [
-          //     {
-          //       transform: `translate3d(${clientX - 30}px, ${clientY - 90}px, 0)`,
-          //     },
-          //     {
-          //       transform: `translate3d(${clientX - 30}px, ${clientY - 50}px, 0)`,
-          //     },
-          //     {
-          //       transform: `translate3d(${clientX - 30}px, ${clientY - 90}px, 0)`,
-          //     },
-          //   ],
-          //   { duration: 300, iterations: 1 },
-          // );
-        }
-      };
+      const currentX = clientX - 30;
+      const currentY = clientY - 90;
+
+      const movingStampImage = new Konva.Image({
+        image: stampImage,
+      });
+      layerRef.current?.add(movingStampImage);
+
+      movingStampImage.show();
+      movingStampImage.setPosition({ x: currentX, y: currentY });
+      movingStampImage.to({
+        x: currentX,
+        y: currentY + 40,
+        onFinish: () => {
+          movingStampImage?.to({
+            x: currentX,
+            y: currentY,
+            onFinish: () => {
+              movingStampImage?.remove();
+            },
+          });
+        },
+      });
 
       drawStampMark(clientX, clientY);
-      stampSeal(clientX, clientY);
       killIfInRange(clientX, clientY);
     };
 
-    const handleMouseMove = (event: Konva.KonvaEventObject<MouseEvent>) => {
-      event.evt.preventDefault();
-      const mouseY = event.evt.clientY;
-      const mouseX = event.evt.clientX;
-      if (imgRef.current) {
-        // imgRef.current.style.transform = `translate3d(${mouseX - 30}px, ${mouseY - 70}px, 0)`;
-      }
-    };
-
     stage.on('mousedown', handleMouseDown);
-    stage.on('mousemove', handleMouseMove);
 
     // Clean up event listeners
     return () => {
       stage.off('mousedown', handleMouseDown);
-      stage.off('mousemove', handleMouseMove);
+      container.style.cursor = 'auto';
     };
   }, [stageRef?.current, ants]);
 
   return (
-    <Layer>
+    <Layer ref={layerRef}>
       {stampPositions.map((position, index) => (
         <KonvaImage
           key={`${index}-key`}
-          image={imageObj}
+          image={stampMarkImage}
           x={position.x - MARK_SIZE / 2}
           y={position.y + 10 - MARK_SIZE / 2}
           width={MARK_SIZE}
@@ -89,7 +88,6 @@ const Paper = ({ stageRef }: PaperProps) => {
           crop={position.cropRect}
         />
       ))}
-      <S.Stamp src="/stamp.png" ref={imgRef} />
     </Layer>
   );
 };
