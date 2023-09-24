64f5a517750c46c3aa3e5223d5d26bd4e7d78c36
haesoo-y <haesoo9410@gmail.com>
2023-09-24 14:55:13 +0900

    feat: useMove훅 적용

---
+++ src/components/Stamp/styled.tsx
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+import { Layer } from 'react-konva';
+import { IMAGE } from '@static';
+
+// const Cursor = styled(Layer)`
+//   position: fixed;
+//   top: 0;
+//   left: 0;
+//   z-index: 9000;
+//   cursor: url(${IMAGE.STAMP_URL}) 8 64, auto;
+// `;
+
+// const NonCursor = styled(Layer)`
+//   position: fixed;
+//   top: 0;
+//   left: 0;
+//   z-index: 9000;
+//   cursor: none;
+// `;
+
+// export default { Cursor, NonCursor };
