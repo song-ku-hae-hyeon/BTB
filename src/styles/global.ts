3efad8642954e149cab1ff6c2c12c0d1e98168f3
haesoo-y <haesoo9410@gmail.com>
2023-09-24 23:15:42 +0900

    refactor: 화면 흔들림 효과 확대

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -4,21 +4,21 @@ const GlobalStyles = createGlobalStyle`
   @keyframes shake {
     10%, 90% {
       background-color: blue;
-      transform: translate3d(-1px, 0, 0);
+      transform: translate3d(-2px, 2px, 0);
     }
     
     20%, 80% {
       background-color: blue;
-      transform: translate3d(2px, 0, 0);
+      transform: translate3d(4px, 8px, 3px);
     }
 
     30%, 50%, 70% {
       background-color: blue;
-      transform: translate3d(-4px, 0, 0);
+      transform: translate3d(-6px, -5px, -3px);
     }
 
     40%, 60% {
-      transform: translate3d(4px, 0, 0);
+      transform: translate3d(6px, 0, 0);
     }
   }
 `;
