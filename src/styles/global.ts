eee26f7c7e2ef81b0f11abb631253768e56ab97e
haesoo-y <haesoo9410@gmail.com>
2023-09-24 23:20:22 +0900

    fix: Test용 코드 제거

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -3,17 +3,14 @@ import { createGlobalStyle } from 'styled-components';
 const GlobalStyles = createGlobalStyle`
   @keyframes shake {
     10%, 90% {
-      background-color: blue;
       transform: translate3d(-2px, 2px, 0);
     }
     
     20%, 80% {
-      background-color: blue;
       transform: translate3d(4px, 8px, 3px);
     }
 
     30%, 50%, 70% {
-      background-color: blue;
       transform: translate3d(-6px, -5px, -3px);
     }
 
