bdb863598dcb28001946708cc1cf3eab55fd533b
ingong <insong.lee@webtoonscorp.com>
2023-10-31 21:46:57 +0900

    chore: bug있던 기존 firework 컴포넌트 제거

--- src/components/index.ts
+++ src/components/index.ts
@@ -5,6 +5,5 @@ export { default as DevBackground } from './DevBackground';
 export { default as Highlighter } from './Highlighter';
 export { default as Stamp } from './Stamp';
 export { default as AntGroup } from './AntGroup';
-export { default as Fireworks } from './Fireworks/Fireworks';
 export { default as ContentWrapper } from './ContentWrapper';
 export { default as Hammer } from './Hammer';
